e6eef1e38ae1c304b6c8bf497d1a2eb7111dc30c
cedo <cedoo@qq.com>
2014-07-28 17:12:49 +0800

    default position

--- src/main/java/cn/com/dhcc/cgn/mobins/action/DefaultAction.java
+++ src/main/java/cn/com/dhcc/cgn/mobins/action/DefaultAction.java
@@ -1,5 +1,7 @@
 package cn.com.dhcc.cgn.mobins.action;
 
+import org.apache.log4j.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class DefaultAction extends ActionSupport{
@@ -7,10 +9,12 @@ public class DefaultAction extends ActionSupport{
 	 * 
 	 */
 	private static final long serialVersionUID = -4538380752869892985L;
+	private static final Logger log = Logger.getLogger(DefaultAction.class.getClass());
 
 	@Override
 	public String execute(){
 		System.out.println("默认action");
+		log.info("跳转到默认页面");
 		return SUCCESS;
 	}
 }
