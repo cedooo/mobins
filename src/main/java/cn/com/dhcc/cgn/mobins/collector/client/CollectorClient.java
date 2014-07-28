562cc225f68246ad1ab08f66ea559bd8d855ac71
cedo <cedoo@qq.com>
2014-07-28 20:49:39 +0800

    add client and client implments

---
+++ src/main/java/cn/com/dhcc/cgn/mobins/collector/client/CollectorClient.java
@@ -0,0 +1,9 @@
+package cn.com.dhcc.cgn.mobins.collector.client;
+/**
+ * 采集客户端
+ * @author CeDo
+ *
+ */
+public abstract class CollectorClient {
+
+}
---
+++ src/main/java/cn/com/dhcc/cgn/mobins/collector/client/impl/CollectorClientImpl.java
@@ -0,0 +1,5 @@
+package cn.com.dhcc.cgn.mobins.collector.client.impl;
+
+public class CollectorClientImpl {
+
+}
