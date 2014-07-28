961000195504a505fb8d9c41138b166db1c7e137
cedo <cedoo@qq.com>
2014-07-28 16:49:56 +0800

    test cast add

---
+++ src/test/java/cn/com/dhcc/cgn/mobins/action/DefaultActionTest.java
@@ -0,0 +1,11 @@
+package cn.com.dhcc.cgn.mobins.action;
+
+import junit.framework.TestCase;
+
+public class DefaultActionTest extends TestCase {
+
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
