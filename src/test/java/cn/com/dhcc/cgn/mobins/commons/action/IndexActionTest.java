f26ee5e3dc939279d6dcf5ac9bc894b1c8b018df
cedo <cedoo@qq.com>
2014-07-28 21:03:36 +0800

    modify test case

--- src/test/java/cn/com/dhcc/cgn/mobins/commons/action/IndexActionTest.java
+++ src/test/java/cn/com/dhcc/cgn/mobins/commons/action/IndexActionTest.java
@@ -1,4 +1,4 @@
-package cn.com.dhcc.cgn.mobins.action;
+package cn.com.dhcc.cgn.mobins.commons.action;
 
 import static org.junit.Assert.*;
 
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import cn.com.dhcc.cgn.mobins.commons.action.IndexAction;
 
-public class DefaultActionTest {
+public class IndexActionTest {
 
 	@Test
 	public void testExecute() {
