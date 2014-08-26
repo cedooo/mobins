774361309fa42fd32d2fbbf10c15683042d9360c
cedo <cedoo@qq.com>
2014-08-26 10:10:28 +0800

    加入告警前提交

--- src/main/java/cn/com/dhcc/cgn/mobins/inspection/client/impl/WSClientImpl.java
+++ src/main/java/cn/com/dhcc/cgn/mobins/inspection/client/impl/WSClientImpl.java
@@ -3,7 +3,6 @@ package cn.com.dhcc.cgn.mobins.inspection.client.impl;
 import org.json.JSONObject;
 
 import cn.com.dhcc.cgn.mobins.inspection.client.Event;
-import cn.com.dhcc.cgn.mobins.inspection.client.IP10101333;
 import cn.com.dhcc.cgn.mobins.inspection.client.WSClient;
 
 public class WSClientImpl extends WSClient {
@@ -13,7 +12,7 @@ public class WSClientImpl extends WSClient {
 		// TODO Auto-generated method stub
 		JSONObject jsonObj = new JSONObject(event);
 		System.out.println(jsonObj.toString());
-		IP10101333 webservice = new IP10101333();
+		//IP10101333 webservice = new IP10101333();
 		
 		return null;
 	}
